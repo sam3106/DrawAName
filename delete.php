5a09033c80f5a36f4b52df6379f2cb79ad77b390
Teron Sampson <32195526+sam3106@users.noreply.github.com>
2018-02-24 18:08:49 -0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ deleteProfile();
 <html>
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<link rel="stylesheet" href="DrawANumber.css">
+<link rel="stylesheet" href="DrawAName.css">
 <title>Draw A Name</title>
 </head>
 <body>
