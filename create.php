62978971345d507aa0f6c9106ad645d8f2aabd98
Teron Sampson <32195526+sam3106@users.noreply.github.com>
2018-02-24 18:06:35 -0500

    Update create.php

--- create.php
+++ create.php
@@ -20,7 +20,7 @@ $number = $_POST["number"];
 <html>
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<link rel="stylesheet" href="PicANumber.css">
+<link rel="stylesheet" href="DrawANumber.css">
 <title>Draw A Name</title>
 </head>
 <body>
